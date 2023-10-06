ae19d1a97733afbb4bac79f7f8f5f97554231255
keyxcode <keyxcode@gmail.com>
2023-10-06 20:23:17 +0700

    Implement list-nth-mod

--- b/hw4.rkt
+++ b/hw4.rkt
@@ -12,3 +12,8 @@
 (define (string-append-map xs suffix)
     (map (lambda (x) (string-append x suffix)) xs))
 
+(define (list-nth-mod xs n)
+    (cond [(< n 0) (error "list-nth-mod: negative number")]
+        [(null? xs) (error "list-nth-mod: empty list")]
+        [#t (let ([i (remainder n (length xs))]) 
+            (car(list-tail xs i)))]))
--- b/hw4test.rkt
+++ b/hw4test.rkt
@@ -25,8 +25,8 @@
                   (list "dan" "dog" "curry" "dog2") 
                   ".jpg") '("dan.jpg" "dog.jpg" "curry.jpg" "dog2.jpg") "string-append-map test")
    
-  ;  ; list-nth-mod test
-  ;  (check-equal? (list-nth-mod (list 0 1 2 3 4) 2) 2 "list-nth-mod test")
+   ; list-nth-mod test
+   (check-equal? (list-nth-mod (list 0 1 2 3 4) 2) 2 "list-nth-mod test")
    
   ;  ; stream-for-n-steps test
   ;  (check-equal? (stream-for-n-steps ones 2) (list 1 1) "stream-for-n-steps test")
