c173fa68f249ae5c5216dd2166866b0bde296ac6
keyxcode <keyxcode@gmail.com>
2023-11-01 18:33:38 +0700

    Use MyTetris

--- c/hw6assignment.rb
+++ c/hw6assignment.rb
@@ -13,14 +13,33 @@ class MyPiece < Piece
 class MyBoard < Board
   # your enhancements here
   def initialize (game)
-    super()
+    @grid = Array.new(num_rows) {Array.new(num_columns)}
     @current_block = MyPiece.next_piece(self)
+    @score = 0
+    @game = game
+    @delay = 500
+  end
+
+  def rotate_180
+    puts "180"
+    draw
   end
 end
 
 class MyTetris < Tetris
   # your enhancements here
+  def set_board
+    @canvas = TetrisCanvas.new
+    @board = MyBoard.new(self)
+    @canvas.place(@board.block_size * @board.num_rows + 3,
+                  @board.block_size * @board.num_columns + 6, 24, 80)
+    @board.draw
+  end
 
+  def key_bindings
+    super()
+    @root.bind('u', proc {@board.rotate_180})
+  end
 end
   
   
